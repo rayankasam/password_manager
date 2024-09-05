a32546e83e93548d37886afd33fd556074cd6aae
Rayan Kasam <rayankasam12@gmail.com>
2024-09-04 20:20:18 -0400

    Added nginx reverse proxy to deal with api call issue

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,16 @@
+pub mod add_password;
 pub mod database;
 pub mod del_password;
-pub mod add_password;
 pub mod get_password;
 pub mod login;
-pub mod new_user;
 pub mod models;
+pub mod new_user;
 pub mod schema;
 pub mod update_password;
-use crate::login::login;
 use crate::add_password::add_password;
 use crate::del_password::del_password;
 use crate::get_password::get_passwords;
+use crate::login::login;
 use crate::new_user::new_user;
 use crate::update_password::update_password;
 use actix_cors::Cors;
@@ -26,20 +26,21 @@ async fn base_url() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     println!("Hello from the server");
     HttpServer::new(|| {
-        App::new()
-            .wrap(
+        {
+            App::new().wrap(
                 Cors::default()
                     .allow_any_origin()
                     .allow_any_method()
                     .allow_any_header(),
             )
-            .route("/", web::get().to(base_url))
-            .route("/add_password", web::post().to(add_password))
-            .route("/del_password", web::delete().to(del_password))
-            .route("/get_password/{query}", web::get().to(get_passwords))
-            .route("/update_password/{id}", web::put().to(update_password))
-            .route("/new_user", web::post().to(new_user))
-            .route("/login", web::post().to(login))
+        }
+        .route("/api/", web::get().to(base_url))
+        .route("/api/add_password", web::post().to(add_password))
+        .route("/api/del_password", web::delete().to(del_password))
+        .route("/api/get_password/{query}", web::get().to(get_passwords))
+        .route("/api/update_password/{id}", web::put().to(update_password))
+        .route("/api/new_user", web::post().to(new_user))
+        .route("/api/login", web::post().to(login))
     })
     .bind("0.0.0.0:8080")?
     .run()
