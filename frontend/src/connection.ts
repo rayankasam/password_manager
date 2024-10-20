8e24e055c3cd962ae8aa97ff8deece4393d2c2cf
Rayan <rayankasam12@gmail.com>
2024-10-20 14:02:46 -0400

    corrected production url

--- frontend/src/connection.ts
+++ frontend/src/connection.ts
@@ -1,1 +1,1 @@
-export const host = import.meta.env.PROD ? "https://pw.rayan.lol/api" : "http://localhost:1234/api"
+export const host = import.meta.env.PROD ? "https://pm.rayan.lol/api" : "http://localhost:1234/api"
